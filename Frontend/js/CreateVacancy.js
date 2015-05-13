c7f9db3310ccda8b2d6d1e2b76c156de983df0c9
loreV <lore.verri@gmail.com>
2015-05-13 15:03:58 +0200

    Added js example

---
+++ Frontend/js/CreateVacancy.js
@@ -0,0 +1,16 @@
+$(document).ready(function(){
+
+    $('#createVacancy').submit(function( event ){
+        console.log(event);
+       // Grab stuff and save it.
+        event.preventDefault();
+
+
+
+
+
+    });
+
+
+
+});
