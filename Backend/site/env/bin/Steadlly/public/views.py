651f7f99c1dc4ec7b3f5d92d6efa02f584755e01
loreV <lore.verri@gmail.com>
2015-05-20 14:26:26 +0200

    Ready for Pull]

--- Backend/site/env/bin/Steadlly/public/views.py
+++ Backend/site/env/bin/Steadlly/public/views.py
@@ -1,7 +1,12 @@
-from django.shortcuts import render
-from models import *
 from django.shortcuts import render, redirect
-from django.template import RequestContext,loader
+from django.views import generic
+from django.template import RequestContext, loader
+from django.contrib import auth
+from django.http import HttpResponse
+from django.core.context_processors import csrf
+# Create your views here.
 
 # Create your views here.
-class IndexView(request):
+def IndexView(request):
+    template = loader.get_template('public/index.html')# << get template
+    return HttpResponse(template.render())
